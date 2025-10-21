df621d0bd5826c81883712500e72aea7d8e5d21b
JOÃO EDUARDO MARTINS ESKELSEN <joao_eskelsen@estudante.sesisenai.org.br>
2025-10-21 16:32:06 -0300

    Nova pagina inserida

--- lib/features/auth/screens/welcomePage.dart
+++ lib/features/auth/screens/welcomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tela_de_cadastro/features/auth/screens/login.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -6,23 +7,34 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, // centraliza na tela
           children: [
-            const Text('Seja bem vindo!'),
-            const SizedBox(height: 20), // espaço entre texto e botão
+            const Text('Seja bem vindo!',  style: TextStyle(fontFamily: 'Poppins', fontSize: 26.0 , fontWeight: FontWeight.w500), ),
+            const SizedBox(height: 90), // espaço entre texto e botão
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: Color(0xFFD49100),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                ),
               ),
               onPressed: () {
-                // ação quando o botão for pressionado
                 print('Botão pressionado!');
+                Navigator.of(context).push(
+                  MaterialPageRoute<void>(
+                    builder: (context) => const Login(),
+                  )
+                );  
               },
-              child: const Text(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: const Text(
                 'Iniciar cadastro',
-                style: TextStyle(fontSize: 16, color: Color(0xFFFFFFFF)),
+                style: TextStyle(fontSize: 16, color: Color(0xFFFFFFFF), fontFamily: 'Poppins', fontWeight: FontWeight.w500 ),
+              ),
               ),
             ),
           ],
